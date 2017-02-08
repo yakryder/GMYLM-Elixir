05a8dc466e7ce20f2982e90b219d65cdd59a319a
SherSpock <wrtimberlake@gmail.com>
2017-02-07 20:26:34 -0500

    Refactored Gmylm.World.Location.remove_object/2 tests

--- test/location_test.exs
+++ test/location_test.exs
@@ -9,6 +9,13 @@ defmodule Gmylm.World.LocationTest do
 
   doctest Location
 
+  setup_all do
+    balloon  = %Object{ground_description: "A deflated balloon rests here, looking sad and unloved."}
+    basement = %Location{description: "The basement's not scary, okay?  Some people say it is but I say it's only scary if there's a storm and the lights are out and something is not put away where you expect it to be and it looks like a person in the shadows.  Otherwise the basement is neat.  It's full of tools and stuff.\n\nThe only way out is back up the stairs."}
+    basement_w_balloon = %Location{basement | on_ground: [balloon]}
+    {:ok, balloon: balloon, basement: basement, basement_w_balloon: basement_w_balloon}
+  end
+
   test "it exists" do
     assert Location.__info__(:functions)
   end
@@ -56,16 +63,14 @@ defmodule Gmylm.World.LocationTest do
       assert_raise FunctionClauseError, fn -> Location.add_object("I am", "defined") end
     end
 
-    test "it adds the specified object to the specified location" do
-      balloon          = %Object{ground_description: "A deflated balloon rests here, looking sad and unloved."}
+    test "it adds the specified object to the specified location", %{balloon: balloon} do
       location         = %Location{}
       updated_location = Location.add_object(balloon, location)
       assert List.first(updated_location.on_ground) == balloon
     end
 
-    test "when adding an object it preserves all the unmodified data of the specified location" do
+    test "when adding an object it preserves the unmodified data of the specified location", %{balloon: balloon, basement: basement} do
       basement_description = "The basement's not scary, okay?  Some people say it is but I say it's only scary if there's a storm and the lights are out and something is not put away where you expect it to be and it looks like a person in the shadows.  Otherwise the basement is neat.  It's full of tools and stuff.\n\nThe only way out is back up the stairs."
-      balloon              = %Object{ground_description: "A deflated balloon rests here, looking sad and unloved."}
       basement             = %Location{description: basement_description}
       updated_basement     = Location.add_object(balloon, basement)
       assert List.first(updated_basement.on_ground) == balloon
@@ -74,24 +79,14 @@ defmodule Gmylm.World.LocationTest do
   end
 
   describe "Location.remove_object/2" do
-    test "it removes the specified object from the specified location" do
-      balloon          = %Object{ground_description: "A deflated balloon rests here, looking sad and unloved."}
-      basement         = %Location{}
-      updated_basement = Location.add_object(balloon, basement)
-      assert List.first(updated_basement.on_ground) == balloon
-      updated_basement = Location.remove_object(balloon, basement)
-      assert updated_basement.on_ground == []
+    test "it removes the specified object from the specified location", %{balloon: balloon, basement_w_balloon: basement_w_balloon} do
+      empty_basement = Location.remove_object(balloon, basement_w_balloon)
+      assert empty_basement.on_ground == []
     end
 
-    test "when removing an object it preserves all the unmodified data of the specified location" do
-      basement_description = "The basement's not scary, okay?  Some people say it is but I say it's only scary if there's a storm and the lights are out and something is not put away where you expect it to be and it looks like a person in the shadows.  Otherwise the basement is neat.  It's full of tools and stuff.\n\nThe only way out is back up the stairs."
-      balloon              = %Object{ground_description: "A deflated balloon rests here, looking sad and unloved."}
-      basement             = %Location{description: basement_description}
-      added_basement       = Location.add_object(balloon, basement)
-      assert List.first(added_basement.on_ground) == balloon
-      assert added_basement.description           == basement_description
-      removed_basement     = Location.remove_object(balloon, added_basement)
-      assert removed_basement.description         == basement_description
+    test "when removing an object it preserves all the unmodified data of the specified location", %{balloon: balloon, basement: basement, basement_w_balloon: basement_w_balloon} do
+      basement_removed_from = Location.remove_object(balloon, basement_w_balloon)
+      assert basement_removed_from.description == basement.description
     end
   end
 end
