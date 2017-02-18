3d12408740ab4f22d26b6548b5e051c63ed9870d
SherSpock <wrtimberlake@gmail.com>
2017-02-17 21:29:11 -0500

    Write them tests, make em pass!

--- lib/object.ex
+++ lib/object.ex
@@ -2,6 +2,7 @@ defmodule Gmylm.World.Object do
   @moduledoc """
   Object data structure and Object functions
   """
+  alias Gmylm.World.Object
 
   @doc """
   Struct for Objects.
@@ -10,21 +11,21 @@ defmodule Gmylm.World.Object do
 
   @doc """
   Initializes the game objects as %Object{} structs.
-  There is some number of objects, and everyone should be an %Object
+  There are 47 objects, and everyone should be an %Object
 
   Examples:
-    iex> Location.initialize_objects |> Enum.count |> is_integer
-    true
+    iex> Object.initialize_objects |> Enum.count
+    47
 
-    iex> Location.initialize_objects |> Enum.all?(fn(element) -> element.__struct__ == Gmylm.World.Object end )
+    iex> Object.initialize_objects |> Enum.all?(fn(element) -> element.__struct__ == Gmylm.World.Object end )
     true
   """
 
   @spec initialize_objects :: [%Object{}]
 
-  def initialize_locations do
-    "lib/object_data.yml"     |>
-    YamlElixir.read_from_file |>
+  def initialize_objects do
+    "lib/object_data.yml"      |>
+    YamlElixir.read_from_file(atoms: true) |>
     Enum.map(fn(object_map) -> Map.merge(%Object{}, object_map) end)
   end
 end
