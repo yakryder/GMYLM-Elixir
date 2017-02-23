9241721b49bfa8bbb1fbbd2e843aeb09acda7caf
SherSpock <wrtimberlake@gmail.com>
2017-02-22 21:00:37 -0500

    Specs! Flowers! Jasmine!

---
+++ web/jasmine/spec/GMYLMSpec.js
@@ -0,0 +1,10 @@
+describe("GMYLM", function() {
+  // beforeEach(function() {
+  //   player = new Player();
+  //   song = new Song();
+  // });
+  //
+  it("should be able to play a Song", function() {
+    expect(true);
+  });
+});
