90ce13f3748658155ec19ce67f27900d176ad417
SherSpock <wrtimberlake@gmail.com>
2017-03-09 21:11:21 -0500

    Created an Event module and test file

---
+++ lib/event.ex
@@ -0,0 +1,39 @@
+defmodule Gmylm.World.Event do
+  @moduledoc """
+  Event data structure and Event functions
+  """
+  alias Gmylm.World.Location
+  alias Gmylm.World.Object
+  alias Gmylm.World.Player
+  alias Gmylm.World.Event
+
+  @doc """
+  Struct for Events.
+  """
+  defstruct name: nil, text: nil, code: nil
+
+  @doc """
+  Initializes the game events as %Event{} structs.
+  There are x events, and every one should be an %Event{}
+
+  Examples:
+    iex> Event.initialize_events |> Enum.count
+    1
+
+    iex> Event.initialize_events |> Gmylm.Helper.all_elements_are?(Gmylm.World.Event)
+    true
+  """
+
+  @spec initialize_events :: [%Event{}]
+
+  def initialize_events do
+    "lib/data/event_data.yml"              |>
+    YamlElixir.read_from_file(atoms: true) |>
+    Enum.map(fn(object_map) -> Map.merge(%Event{}, object_map) end)
+  end
+
+  def run do
+    # Display text
+    # Run associated code if it exists
+  end
+end
---
+++ test/event_test.exs
@@ -0,0 +1,15 @@
+defmodule Gmylm.World.EventTest do
+  @moduledoc """
+    Tests the GMYLM Object functions and data
+  """
+  use ExUnit.Case, async: true
+  alias Gmylm.Player
+  alias Gmylm.World.Location
+  alias Gmylm.World.Object
+  alias Gmylm.World.Event
+
+  doctest Event
+  describe "intialize_events/2" do
+    
+  end
+end
