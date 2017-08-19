a441d1f0feb8ad16d4836f088c93337c582420e3
SherSpock <wrtimberlake@gmail.com>
2017-08-19 18:44:30 -0400

    Started acceptance test

--- test/gmylm_test.exs
+++ test/gmylm_test.exs
@@ -13,8 +13,6 @@ defmodule GmylmTest do
   alias Gmylm.World.Event
   alias Gmylm.Interface
 
-  # doctest Gmylm
-
   setup_all do
     game = Gmylm.initialize_game
     { _status, _player, world} = game
@@ -22,22 +20,44 @@ defmodule GmylmTest do
     {:ok, game: game, locations: locations, objects: objects, events: events, world: world}
   end
 
-  test "it exists" do
-    assert Gmylm.__info__(:functions)
-  end
 
-  test "it has a World module" do
-    assert Gmylm.World.__info__(:functions)
-  end
+  # Problem Description:
+  # Game loop function is recursive
+  # Need to be able to pass instructions from within game loop but my test code can't get there
+  
+  # One solution:
+  # Pass a list of instructions to one definition of game_loop/3
+  # When game_loop/3 receives a list of instructions, invoke the instruction then call itself with the invoked instruction removed
+  # Maybe pass a list of tuples where first element is instruction and second is assertions
 
-  test "it has a Player module" do
-    assert Gmylm.Player.__info__(:functions)
-  end
+  describe "Acceptance Test" do
+    # test "it plays through the game" do
+    #    {_, player, world} = Gmylm.initialize_game
+    #    Gmylm.game_loop(player, world)
 
-  describe "game_loop/2" do
-    # test "it has a prompt", %{game: game} do
-    #   assert capture_io(Gmylm.game_loop(%Player{}, world, true)) =~ "> "
-    # end
+    # end  
+  end  
+
+  # test "it exists" do
+  #   assert Gmylm.__info__(:functions)
+  # end
+
+  # test "it has a World module" do
+  #   assert Gmylm.World.__info__(:functions)
+  # end
+
+  # test "it has a Player module" do
+  #   assert Gmylm.Player.__info__(:functions)
+  # end
+
+  # describe "game_loop/2" do
+  #   test "it has a prompt", %{game: game, world: world} do
+  #     assert capture_io(Gmylm.game_loop(%Player{}, world)) =~ "> "
+  #     Gmylm.game_loop(%Player{}, world, "victory")
+  #   end
+  # end
+
+    
   end
 
   describe "initialize_game/0" do
