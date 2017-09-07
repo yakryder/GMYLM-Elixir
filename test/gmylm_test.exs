76fdc95e1e79d281093b43f7b9ad5c47d374995e
SherSpock <wrtimberlake@gmail.com>
2017-09-06 21:02:18 -0400

    Get rid of some garbage tests. Put in some stubs and TODOS

--- test/gmylm_test.exs
+++ test/gmylm_test.exs
@@ -15,9 +15,9 @@ defmodule GmylmTest do
 
   setup_all do
     game = Gmylm.initialize_game
-    { _status, _player, world} = game
+    { _status, player, world} = game
     %World{locations: locations, objects: objects, events: events} = world
-    {:ok, game: game, locations: locations, objects: objects, events: events, world: world}
+    {:ok, game: game, player: player, locations: locations, objects: objects, events: events, world: world}
   end
 
 
@@ -30,34 +30,34 @@ defmodule GmylmTest do
   # When game_loop/3 receives a list of instructions, invoke the instruction then call itself with the invoked instruction removed
   # Maybe pass a list of tuples where first element is instruction and second is assertions
 
-  describe "Acceptance Test" do
-    # test "it plays through the game" do
-    #    {_, player, world} = Gmylm.initialize_game
-    #    Gmylm.game_loop(player, world)
+  # describe "acceptance test" do
+  #   test "it plays through the game" do
+  #      {_, player, world} = Gmylm.initialize_game
+  #      {_, player, world} = Gmylm.game_loop(player, world)
 
-    # end  
-  end  
+  #   end  
+  # end  
 
-  # test "it exists" do
-  #   assert Gmylm.__info__(:functions)
-  # end
+  # Maybe player should be part of world (like as a player key of the world struct)
 
-  # test "it has a World module" do
-  #   assert Gmylm.World.__info__(:functions)
-  # end
+  describe "game_loop/3" do
+  #   test "it has a prompt", %{world: world} do
+  #           Gmylm.game_loop(%Player{}, world, "victory")
 
-  # test "it has a Player module" do
-  #   assert Gmylm.Player.__info__(:functions)
-  # end
-
-  # describe "game_loop/2" do
-  #   test "it has a prompt", %{game: game, world: world} do
   #     assert capture_io(Gmylm.game_loop(%Player{}, world)) =~ "> "
   #     Gmylm.game_loop(%Player{}, world, "victory")
   #   end
-  # end
+  end
 
-    
+
+
+  # Hey this is to mock some process command calls
+  describe "process_command/3" do
+    defmodule CustomAdapter do
+      def process_command(input, player, world) do
+        [input, player, world]
+      end
+    end
   end
 
   describe "initialize_game/0" do
