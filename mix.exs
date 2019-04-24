f405782caa9a55941623fa3e024a48ca8c15fae8
SherSpock <wrtimberlake@gmail.com>
2019-04-24 01:00:13 -0400

    Version up and pin deps
    Was running afoul of Credo issue
    https://github.com/rrrene/credo/issues/463 with Elixir 1.8.1 locally.
    
    Took it as an opportunity to pin versions and get on last-month
    versions of stuff

--- mix.exs
+++ mix.exs
@@ -31,9 +31,9 @@ defmodule Gmylm.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:credo, "~> 0.5", only: [:dev, :test]},
-     {:yaml_elixir, "~> 1.3.0"},
-     {:ex_doc, "~> 0.14", only: :dev, runtime: false}]
+    [{:yaml_elixir, "2.1.0"},
+     {:credo, "1.0.4", only: [:dev, :test]},
+     {:ex_doc, "0.20.0", only: :dev, runtime: false}]
   end
 
   # defp elixirc_paths(:test), do: ["lib", "test/test_helpers"]
